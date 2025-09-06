ac355724eeda1ea56ce81eae1868b0cdb98b9e1e
Arshad vkb <arshadvkb118@gmail.com>
2025-09-06 14:40:01 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -39,7 +39,7 @@ const Contact = () => {
                 </div>
                 <div className="flex items-center gap-4">
                   <MapPin className="w-5 h-5 text-primary" />
-                  <span>Available for Remote Work</span>
+                  <span>Calicut, Kerala </span>
                 </div>
                 <div className="flex items-center gap-4">
                   <Calendar className="w-5 h-5 text-primary" />
